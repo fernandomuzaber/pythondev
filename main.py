c058f3e62b5527383cfde405b005b506fb45a3a7
Fernando Muzaber <fmuzaber@gmail.com>
2025-01-28 15:04:26 -0300

    first python program

---
+++ main.py
@@ -0,0 +1,10 @@
+#First Python program
+print("Hello World! ")
+
+print("Welcome to the Band Name Generator.")
+
+name_city = input("What's the name of the city you grew up in?\n")
+
+name_pet = input("What's your pet's name?\n")
+
+print("Your band name could be: " + name_city + " " + name_pet)
