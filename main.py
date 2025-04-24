1faffa7871674850ae5d12b4df14af0514bf9e66
Fernando Muzaber <fmuzaber@gmail.com>
2025-04-24 14:39:40 -0300

    python game treasure Island

--- main.py
+++ main.py
@@ -1,16 +1,74 @@
-print("Welcome to the tip calculator!\n")
+print(r'''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\ ` . "-._ /_______________|_______
+|                   | |o ;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
 
-total_bill = float(input("What was the total bill? $"))
+direction = input('''
+You're at a cross road. Where do you want to go?
+    Type "left" or "right"
+\n''').lower()
 
-tip_percent = float(input("How much tip would you like to give? 10, 12, or 15%?"))
+if direction == "left":
+    swim_or_wait = input('''
+    You've come to a lake, there is an island in the middle of the lake.                        
+    Do you want to swim or do you prefer to wait?
+        Insert "swim" for swim or "wait" for wait
+    \n''').lower()
+    if swim_or_wait == "wait":
+        door = input('''
+        You arrive at the island unharmed. There is a house with 3 doors.
+        You have to choose one door to enter:
+            Please select into: red, yellow or blue
+        \n''').lower()
+        if door == "red":
+            print('''
+            It's a room full of fire.
+            Burned by fire.
+            Game Over
+            \n''')
+        elif door == "blue":
+            print('''
+            You enter a room of beasts.
+            Game Over.
+            \n''')
+        elif door == "yellow":
+            print('''
+            Congratulations! You found the treasure.
+            You win the game
+            \n''')
+        else:
+            print('You choose a door that doesn\'t exist. Game over\n')
 
-tip_percent /= 100 
+    else:
+        print('''
+        You were attacked by an angry trout.
+        Game Over.
+        ''')
+else:
+    print('''
+    Fall into a hole.
+    Game Over.
+    ''')
 
-people = float(input("How many people to split the bill?"))
 
-result = total_bill *  (tip_percent+1) / people
-
-formatted_result = "{:.2f}".format(result)
-
-print(f"Each person should pay: ${formatted_result }")
 
