2f7022d3dc2c63269b188af56ae5e901618b3199
Fernando Muzaber <fmuzaber@gmail.com>
2025-05-05 09:21:39 -0300

    Rock, paper & scissors python game

--- main.py
+++ main.py
@@ -1,198 +1,60 @@
-# print(r'''
-# *******************************************************************************
-#           |                   |                  |                     |
-#  _________|________________.=""_;=.______________|_____________________|_______
-# |                   |  ,-"_,=""     `"=.|                  |
-# |___________________|__"=._o`"-._        `"=.______________|___________________
-#           |                `"=._o`"=._      _`"=._                     |
-#  _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
-# |                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
-# |___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
-#           |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
-#  _________|___________| ;`-.o`"=._; ." ` '`."\ ` . "-._ /_______________|_______
-# |                   | |o ;    `"-.o`"=._``  '` " ,__.--o;   |
-# |___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
-# ____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
-# /______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
-# ____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
-# /______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
-# ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
-# /______/______/______/______/______/______/______/______/______/______/_____ /
-# *******************************************************************************
-# ''')
-# print("Welcome to Treasure Island.")
-# print("Your mission is to find the treasure.")
+import random
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
 
-# direction = input('''
-# You're at a cross road. Where do you want to go?
-#     Type "left" or "right"
-# \n''').lower()
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
 
-# if direction == "left":
-#     swim_or_wait = input('''
-#     You've come to a lake, there is an island in the middle of the lake.                        
-#     Do you want to swim or do you prefer to wait?
-#         Insert "swim" for swim or "wait" for wait
-#     \n''').lower()
-#     if swim_or_wait == "wait":
-#         door = input('''
-#         You arrive at the island unharmed. There is a house with 3 doors.
-#         You have to choose one door to enter:
-#             Please select into: red, yellow or blue
-#         \n''').lower()
-#         if door == "red":
-#             print('''
-#             It's a room full of fire.
-#             Burned by fire.
-#             Game Over
-#             \n''')
-#         elif door == "blue":
-#             print('''
-#             You enter a room of beasts.
-#             Game Over.
-#             \n''')
-#         elif door == "yellow":
-#             print('''
-#             Congratulations! You found the treasure.
-#             You win the game
-#             \n''')
-#         else:
-#             print('You choose a door that doesn\'t exist. Game over\n')
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
 
-#     else:
-#         print('''
-#         You were attacked by an angry trout.
-#         Game Over.
-#         ''')
-# else:
-#     print('''
-#     Fall into a hole.
-#     Game Over.
-#     ''')
-
-# Mersenne Twister
-
-# import sys
-# sys.set_int_max_str_digits(10000)  # o más, según lo que necesites
-
-# print(2**19937)
-
-# import random
-# import my_module
-
-# # from a to B
-# rand_num = random.randint(1,10)
-
-# print(rand_num)
-
-# # random not includes 1
-# rand_num_0_to_1 = random.random() * 5
-
-# print(rand_num_0_to_1)
-
-# #uniform includes the b
-# random_float = random.uniform(1, 10)
-
-# print(random_float)
-
-# #Importo el modulo creado y uso la variable creada en el modulo
-# print(my_module.my_favourite_number)
-
-
-# import random
-# import my_module
-
-# from a to B
-# rand_num = random.randint(1,10)
-
-# print(rand_num)
-
-#  It generates a random number between 0 and 1, but 1 is not included
-# rand_num_0_to_1 = random.random() * 5
-
-# print(rand_num_0_to_1)
-
-#uniform includes the b
-# random_float = random.uniform(1, 10)
-
-# print(random_float)
-# print(my_module.my_favourite_number)
-
-# Head or tails
-
-# if random.random() * 10 > 5:
-#     print("Heads")
-# else:
-#     print("Tails")
-
-# Her solution
-
-# random_heads_or_tails = random.randint(0,1)
-
-# if(random_heads_or_tails == 0):
-#     print("Heads")
-# else:
-#     print("Tails")
-
-# Lists examples
-
-# states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
-# print(states_of_america[0])
-# print(states_of_america[-1])
-# states_of_america[0] = "Deloitteware"
-# print(states_of_america)
-# states_of_america[0] = "Delaware"
-# print(states_of_america)
-# states_of_america.extend(["Angelaland", "Jack Bauer Land"])
-# print(states_of_america)
-# states_of_america.pop()
-# states_of_america.pop()
-# print(states_of_america)
-# print(states_of_america.index("Georgia"))
-
-# Delaware
-# Hawaii
-# ['Deloitteware', 'Pennsylvania', 'New Jersey', 'Georgia', 'Connecticut', 'Massachusetts', 'Maryland', 'South Carolina', 'New Hampshire', 'Virginia', 'New York', 'North Carolina', 'Rhode Island', 'Vermont', 'Kentucky', 'Tennessee', 'Ohio', 'Louisiana', 'Indiana', 'Mississippi', 'Illinois', 'Alabama', 'Maine', 'Missouri', 'Arkansas', 'Michigan', 'Florida', 'Texas', 'Iowa', 'Wisconsin', 'California', 'Minnesota', 'Oregon', 'Kansas', 'West Virginia', 'Nevada', 'Nebraska', 'Colorado', 'North Dakota', 'South Dakota', 'Montana', 'Washington', 'Idaho', 'Wyoming', 'Utah', 'Oklahoma', 'New Mexico', 'Arizona', 'Alaska', 'Hawaii']
-# ['Delaware', 'Pennsylvania', 'New Jersey', 'Georgia', 'Connecticut', 'Massachusetts', 'Maryland', 'South Carolina', 'New Hampshire', 'Virginia', 'New York', 'North Carolina', 'Rhode Island', 'Vermont', 'Kentucky', 'Tennessee', 'Ohio', 'Louisiana', 'Indiana', 'Mississippi', 'Illinois', 'Alabama', 'Maine', 'Missouri', 'Arkansas', 'Michigan', 'Florida', 'Texas', 'Iowa', 'Wisconsin', 'California', 'Minnesota', 'Oregon', 'Kansas', 'West Virginia', 'Nevada', 'Nebraska', 'Colorado', 'North Dakota', 'South Dakota', 'Montana', 'Washington', 'Idaho', 'Wyoming', 'Utah', 'Oklahoma', 'New Mexico', 'Arizona', 'Alaska', 'Hawaii']
-# ['Delaware', 'Pennsylvania', 'New Jersey', 'Georgia', 'Connecticut', 'Massachusetts', 'Maryland', 'South Carolina', 'New Hampshire', 'Virginia', 'New York', 'North Carolina', 'Rhode Island', 'Vermont', 'Kentucky', 'Tennessee', 'Ohio', 'Louisiana', 'Indiana', 'Mississippi', 'Illinois', 'Alabama', 'Maine', 'Missouri', 'Arkansas', 'Michigan', 'Florida', 'Texas', 'Iowa', 'Wisconsin', 'California', 'Minnesota', 'Oregon', 'Kansas', 'West Virginia', 'Nevada', 'Nebraska', 'Colorado', 'North Dakota', 'South Dakota', 'Montana', 'Washington', 'Idaho', 'Wyoming', 'Utah', 'Oklahoma', 'New Mexico', 'Arizona', 'Alaska', 'Hawaii', 'Angelaland', 'Jack Bauer Land']
-# ['Delaware', 'Pennsylvania', 'New Jersey', 'Georgia', 'Connecticut', 'Massachusetts', 'Maryland', 'South Carolina', 'New Hampshire', 'Virginia', 'New York', 'North Carolina', 'Rhode Island', 'Vermont', 'Kentucky', 'Tennessee', 'Ohio', 'Louisiana', 'Indiana', 'Mississippi', 'Illinois', 'Alabama', 'Maine', 'Missouri', 'Arkansas', 'Michigan', 'Florida', 'Texas', 'Iowa', 'Wisconsin', 'California', 'Minnesota', 'Oregon', 'Kansas', 'West Virginia', 'Nevada', 'Nebraska', 'Colorado', 'North Dakota', 'South Dakota', 'Montana', 'Washington', 'Idaho', 'Wyoming', 'Utah', 'Oklahoma', 'New Mexico', 'Arizona', 'Alaska', 'Hawaii']
-# PS C:\root\Computacion\Desarrollo\Python\Udemy\100 days of Code - Python Pro Bootcamp Angela Yu\VSC repo python> python -u "c:\root\Computacion\Desarrollo\Python\Udemy\100 days of Code - Python Pro Bootcamp Angela Yu\VSC repo python\main.py"
-# Delaware
-# Hawaii
-# ['Deloitteware', 'Pennsylvania', 'New Jersey', 'Georgia', 'Connecticut', 'Massachusetts', 'Maryland', 'South Carolina', 'New Hampshire', 'Virginia', 'New York', 'North Carolina', 'Rhode Island', 'Vermont', 'Kentucky', 'Tennessee', 'Ohio', 'Louisiana', 'Indiana', 'Mississippi', 'Illinois', 'Alabama', 'Maine', 'Missouri', 'Arkansas', 'Michigan', 'Florida', 'Texas', 'Iowa', 'Wisconsin', 'California', 'Minnesota', 'Oregon', 'Kansas', 'West Virginia', 'Nevada', 'Nebraska', 'Colorado', 'North Dakota', 'South Dakota', 'Montana', 'Washington', 'Idaho', 'Wyoming', 'Utah', 'Oklahoma', 'New Mexico', 'Arizona', 'Alaska', 'Hawaii']
-# ['Delaware', 'Pennsylvania', 'New Jersey', 'Georgia', 'Connecticut', 'Massachusetts', 'Maryland', 'South Carolina', 'New Hampshire', 'Virginia', 'New York', 'North Carolina', 'Rhode Island', 'Vermont', 'Kentucky', 'Tennessee', 'Ohio', 'Louisiana', 'Indiana', 'Mississippi', 'Illinois', 'Alabama', 'Maine', 'Missouri', 'Arkansas', 'Michigan', 'Florida', 'Texas', 'Iowa', 'Wisconsin', 'California', 'Minnesota', 'Oregon', 'Kansas', 'West Virginia', 'Nevada', 'Nebraska', 'Colorado', 'North Dakota', 'South Dakota', 'Montana', 'Washington', 'Idaho', 'Wyoming', 'Utah', 'Oklahoma', 'New Mexico', 'Arizona', 'Alaska', 'Hawaii']
-# ['Delaware', 'Pennsylvania', 'New Jersey', 'Georgia', 'Connecticut', 'Massachusetts', 'Maryland', 'South Carolina', 'New Hampshire', 'Virginia', 'New York', 'North Carolina', 'Rhode Island', 'Vermont', 'Kentucky', 'Tennessee', 'Ohio', 'Louisiana', 'Indiana', 'Mississippi', 'Illinois', 'Alabama', 'Maine', 'Missouri', 'Arkansas', 'Michigan', 'Florida', 'Texas', 'Iowa', 'Wisconsin', 'California', 'Minnesota', 'Oregon', 'Kansas', 'West Virginia', 'Nevada', 'Nebraska', 'Colorado', 'North Dakota', 'South Dakota', 'Montana', 'Washington', 'Idaho', 'Wyoming', 'Utah', 'Oklahoma', 'New Mexico', 'Arizona', 'Alaska', 'Hawaii', 'Angelaland', 'Jack Bauer Land']
-# ['Delaware', 'Pennsylvania', 'New Jersey', 'Georgia', 'Connecticut', 'Massachusetts', 'Maryland', 'South Carolina', 'New Hampshire', 'Virginia', 'New York', 'North Carolina', 'Rhode Island', 'Vermont', 'Kentucky', 'Tennessee', 'Ohio', 'Louisiana', 'Indiana', 'Mississippi', 'Illinois', 'Alabama', 'Maine', 'Missouri', 'Arkansas', 'Michigan', 'Florida', 'Texas', 'Iowa', 'Wisconsin', 'California', 'Minnesota', 'Oregon', 'Kansas', 'West Virginia', 'Nevada', 'Nebraska', 'Colorado', 'North Dakota', 'South Dakota', 'Montana', 'Washington', 'Idaho', 'Wyoming', 'Utah', 'Oklahoma', 'New Mexico', 'Arizona', 'Alaska', 'Hawaii']
-# 3
-
-# import random
-
-# friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
-
-# # Two options
-# # 1st one
-
-# print(f"First one: ", {random.choice(friends)})
-
-# # 2nd one
-
-# random_index = random.randint(0,4)
-# print(f"Second one: ", friends[random_index])
-
-# # Mine
-# print(friends[int(random.random() * 5)])
-
-dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"]
-
-print(len(dirty_dozen)) #12
-
-# print(dirty_dozen[12]) # => IndexError because the lists begin in zero. You have to subtract one
-print(dirty_dozen[len(dirty_dozen) - 1])
-
-# Nested lists
-
-fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
-vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
-
-dirty_dozen = [fruits, vegetables]
-print(dirty_dozen[1][1]) #Kale 
+values = [rock, paper, scissors]
+computer_election = random.randint(0, 2)
+election = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n")
+if election == "0":
+    print(values[0])
+    print(values[computer_election])
+    if computer_election == 0:
+        print("It's a draw!")
+    elif computer_election == 1:
+        print("You lose!")
+    else:
+        print("You win!")
+elif election == "1":
+    print(values[1])
+    print(values[computer_election])
+    if computer_election == 0:
+        print("You win!")
+    elif computer_election == 1:
+        print("It's a draw!")
+    else:
+        print("You lose!")
+elif election == "2":
+    print(scissors)
+    print(values[computer_election])
+    if computer_election == 0:
+        print("You lose!")
+    elif computer_election == 1:
+        print("You win!")
+    else:
+        print("It's a draw!")
+else:
+    print("You typed an invalid number, you lose!")
