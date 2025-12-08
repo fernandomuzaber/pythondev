c3a3c2bcc044311aa3856da36e7da2450079d0e0
Fernando Muzaber <fmuzaber@gmail.com>
2025-12-08 09:45:41 -0300

    Caesar cipher program v1.0

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
-#Love calculator challenge
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
 
 
-def calculate_love_score(name1, name2):
-    true = "TRUE"
-    love = "LOVE"
-    total_words = (name1 + name2)
-    count_true = 0
-    count_love = 0
-    
-    for char in total_words:
-        if char in true.lower():
-            count_true +=1
-        if char in love.lower():
-            count_love +=1
-    final_count = ""
-    final_count = str(count_true) + str(count_love)
-    print(final_count)
-            
-    
-calculate_love_score("John", "Doe")
+# TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
+
+# TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
+#  by the shift amount and print the encrypted text.
+
+# TODO-3: Call the 'encrypt()' function and pass in the user inputs. You should be able to test the code and encrypt a
+#  message.
+
+# TODO-4: What happens if you try to shift z forwards by 9? Can you fix the code?
+
+def encrypt(original_text, shift_amount):
+    cipher_text = ""
+    for letter in original_text:
+        shifted_position = alphabet.index(letter) + shift_amount
+        # Using module function because the alphabet has 25 values, and when I shift by more than that value it makes a console error because it exceeds the maximum value.
+        shifted_position %= len(alphabet)
+        cipher_text += alphabet[shifted_position]
+
+    print(f"Here is the encoded result: {cipher_text}")
+encrypt(original_text = text,shift_amount= shift)
