a17cb2c416bab646a0a58f9f5c567da924e9b4e5
Fernando Muzaber <fmuzaber@gmail.com>
2025-12-08 10:09:37 -0300

    Caesar cipher program v1.1

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ def caesar(original_text, shift_amount, encode_or_decode):
     for letter in original_text:
         if encode_or_decode == 'decode':
             shift_amount *= -1
+
         shifted_position = alphabet.index(letter) + shift_amount
         shifted_position %= len(alphabet)
         output_text += alphabet[shifted_position]
-    print(f"Here is the encoded result: {output_text}")
+    print(f"Here is the {encode_or_decode}d result: {output_text}")
 
 caesar(original_text = text, shift_amount = shift, encode_or_decode = direction)
