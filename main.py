e343fdec51f52c4b39007563a3fa17395df1ab0e
Fernando Muzaber <fmuzaber@gmail.com>
2025-12-08 10:03:38 -0300

    Caesar cipher program v1.1

--- main.py
+++ main.py
@@ -5,39 +5,14 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-# TODO-1: Create a function called 'decrypt()' that takes 'original_text' and 'shift_amount' as inputs.
-
-def decrypt(original_text, shift_amount):
+def caesar(original_text, shift_amount, encode_or_decode):
     output_text = ""
     for letter in original_text:
-        shifted_position = alphabet.index(letter) - shift_amount
-        shifted_position %= len(alphabet)
-        output_text += alphabet[shifted_position]
-    print(f"Here is the decoded result: {output_text}")
-
-# TODO-2: Inside the 'decrypt()' function, shift each letter of the 'original_text' *backwards* in the alphabet
-#  by the shift amount and print the decrypted text.
-# TODO-3: Combine the 'encrypt()' and 'decrypt()' functions into one function called 'caesar()'.
-#  Use the value of the user chosen 'direction' variable to determine which functionality to use.
-
-def encrypt(original_text, shift_amount):
-    output_text = ""
-    for letter in original_text:
+        if encode_or_decode == 'decode':
+            shift_amount *= -1
         shifted_position = alphabet.index(letter) + shift_amount
         shifted_position %= len(alphabet)
         output_text += alphabet[shifted_position]
     print(f"Here is the encoded result: {output_text}")
 
-
-def caesar(original_text, shift_amount, action):
-    output_text = ""
-    if direction == "encode":
-        encrypt(original_text, shift_amount)
-    elif direction == "decode":
-        decrypt(original_text, shift_amount)
-    else:
-        print("Please enter 'encode' or 'decode'")
-
-caesar(original_text = text, shift_amount = shift, action = direction)
-
-
+caesar(original_text = text, shift_amount = shift, encode_or_decode = direction)
