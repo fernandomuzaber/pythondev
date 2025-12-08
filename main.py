ec339d64d5a2082fbe2b507848c00c730893f4ba
Fernando Muzaber <fmuzaber@gmail.com>
2025-12-08 09:54:20 -0300

    Caesar cipher program v1.1

--- main.py
+++ main.py
@@ -5,23 +5,38 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-# TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
+# TODO-1: Create a function called 'decrypt()' that takes 'original_text' and 'shift_amount' as inputs.
 
-# TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
-#  by the shift amount and print the encrypted text.
+def decrypt(original_text, shift_amount):
+    cipher_text = ""
+    for letter in original_text:
+        shifted_position = alphabet.index(letter) - shift_amount
+        shifted_position %= len(alphabet)
+        cipher_text += alphabet[shifted_position]
+    print(f"Here is the decoded result: {cipher_text}")
 
-# TODO-3: Call the 'encrypt()' function and pass in the user inputs. You should be able to test the code and encrypt a
-#  message.
-
-# TODO-4: What happens if you try to shift z forwards by 9? Can you fix the code?
+# TODO-2: Inside the 'decrypt()' function, shift each letter of the 'original_text' *backwards* in the alphabet
+#  by the shift amount and print the decrypted text.
+# TODO-3: Combine the 'encrypt()' and 'decrypt()' functions into one function called 'caesar()'.
+#  Use the value of the user chosen 'direction' variable to determine which functionality to use.
 
 def encrypt(original_text, shift_amount):
     cipher_text = ""
     for letter in original_text:
         shifted_position = alphabet.index(letter) + shift_amount
-        # Using module function because the alphabet has 25 values, and when I shift by more than that value it makes a console error because it exceeds the maximum value.
         shifted_position %= len(alphabet)
         cipher_text += alphabet[shifted_position]
-
     print(f"Here is the encoded result: {cipher_text}")
-encrypt(original_text = text,shift_amount= shift)
+
+
+def caesar(original_text, shift_amount, action):
+    cipher_text = ""
+    if direction == "encode":
+        encrypt(original_text, shift_amount)
+    elif direction == "decode":
+        decrypt(original_text, shift_amount)
+    else:
+        print("Please enter 'encode' or 'decode'")
+
+caesar(original_text = text, shift_amount = shift, action = direction)
+
